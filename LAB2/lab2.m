8c52bf1bd91a83daa4af1e9cbe1ae4f74b69947e
maxkf <45897724+maxkf@users.noreply.github.com>
2023-04-19 00:00:24 +0200

    plot Lab2 update

--- LAB2/lab2.m
+++ LAB2/lab2.m
@@ -1,17 +1,32 @@
-
 %clear all; clc;close all; instrreset
-
-
 %data = serial_datalog('COM9',{'2*single','2*single'}, 'baudrate',115200)
+
 subplot(2,1,1);
-plot(turtlebot_data.time,turtlebot_data.tilt_data);
+yyaxis left
+plot(turtlebot_data.time,turtlebot_data.tilt_data(1,:));
+ylim([-55 55])
+ylabel("angle [deg]");
+
+yyaxis right
+plot(turtlebot_data.time,turtlebot_data.tilt_data(2,:));
+ylim([-11 11])
+ylabel("acceleration [m/s^2]");
+
+xlabel("time [s]");
 grid on;
-xlabel("time (s)");
-ylabel("acceleration/angle");
-legend("tilt (deg)", "a-y (m/s^2)");
+legend("tilt", "acc y-axis");
 
 subplot(2,1,2);
-plot(turtlebot_data.time,turtlebot_data.pan_data);
+yyaxis left
+plot(turtlebot_data.time,turtlebot_data.pan_data(1,:));
+ylim([-35 35])
+ylabel("angle [deg]");
+
+yyaxis right
+plot(turtlebot_data.time,turtlebot_data.pan_data(2,:));
+ylim([-7 7])    
+ylabel("rotational acceleration [deg/s^2]");
+
+xlabel("time [s]");
 grid on;
-ylabel("acceleration/angle");
-legend("pan (deg)", "g-z (deg/s^2)");
+legend("pan", "gyro z-axis");
